58d3d115f14f46004ff91c2b40e0ae5665a6f0cd
bonniesimmer <noura19918@gmail.com>
2025-08-30 11:48:51 +0300

    Added employee attributes and used lombok annotations for cleaner looking code

--- src/main/java/com/example/Springlab/Entity/Employee.java
+++ src/main/java/com/example/Springlab/Entity/Employee.java
@@ -1,4 +1,20 @@
 package com.example.Springlab.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Double salary;
 }
