95038927e54f9d512df3f1a6268fe9a19b40b30f
bonniesimmer <noura19918@gmail.com>
2025-08-30 12:10:06 +0300

    Created the EmployeeService and added the required CRUD services

--- src/main/java/com/example/Springlab/Service/EmployeeService.java
+++ src/main/java/com/example/Springlab/Service/EmployeeService.java
@@ -1,4 +1,57 @@
 package com.example.Springlab.Service;
 
+import com.example.Springlab.Entity.Employee;
+import com.example.Springlab.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepo employeeRepo;
+    Long id_counter = 1L;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee CreateEmployee(String firstName, String lastName, String email, Double salary) {
+        Employee employee = new Employee(id_counter++, firstName, lastName, email, salary);
+        employeeRepo.save(employee);
+        return employee;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepo.findAll();
+    }
+    public Employee GetEmployeeById(Long id) {
+        return employeeRepo.findById(id);
+    }
+
+    public Employee UpdateEmployee(Long id, String firstName, String lastName, String email, Double salary) {
+        Employee existingEmployee = employeeRepo.findById(id);
+        if (existingEmployee != null) {
+            existingEmployee.setFirstName(firstName);
+            existingEmployee.setLastName(lastName);
+            existingEmployee.setEmail(email);
+            existingEmployee.setSalary(salary);
+            employeeRepo.save(existingEmployee);
+            return existingEmployee;
+        } else {
+            throw new RuntimeException("Employee not found with id: " + id);
+        }
+    }
+
+    public void DeleteEmployee(Long id) {
+        if (employeeRepo.existsById(id)) {
+            employeeRepo.deleteById(id);
+        } else {
+            throw new RuntimeException("Employee not found with id: " + id);
+        }
+    }
+
+
 }
